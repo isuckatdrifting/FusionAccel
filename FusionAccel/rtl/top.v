d232f7c454bb5b1a745a5599fd5f7ec265a63fc5
GeraltShi <paladin.heleth@gmail.com>
2019-01-15 12:27:04 +0800

    Add conv, csb and dma

--- FusionAccel/rtl/top.v
+++ FusionAccel/rtl/top.v
@@ -5,55 +5,13 @@ module top(
 //--------------v1, Minimum Hardware Cores for SqueezeNet------------------//
 csb csb_(); //Control Bus for all cores
 conv_3x3 conv_(); //Convolutional Core
-acti acti_(); //Activation Core
 pool pool_(); //Pooling Core
 dma dma_(); //Direct Memory Access Core
-reshape reshape_(); //Memory Reshape and Concatenation Core
+//reshape reshape_(); //Memory Reshape and Concatenation Core
 usb usb_(); //USB Slave
 //--------------v2, More complicate Cores for Other Function and Networks--//
-lut lut_(); //Loop up table for activation and lrn cores
-lrn lrn_(); //Local response normalization Core
-
-endmodule
-
-module csb(
-
-);
-
-endmodule
-
-module acti(
-
-);
-
-endmodule
-
-module pool (
-
-);
-
-endmodule
-
-module lrn(
-
-);
-
-endmodule
-
-module reshape(
-
-);
-
-endmodule
-
-module dma(
-
-);
-
-endmodule
-
-module lut(
-
-);
+//acti acti_(); //Activation Core
+//lut lut_(); //Loop up table for activation and lrn cores
+//lrn lrn_(); //Local response normalization Core
 
 endmodule
