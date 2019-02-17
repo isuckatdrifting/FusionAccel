3025fb07f5044f368bcdd3aea3e6ef623595d4ff
GeraltShi <paladin.heleth@gmail.com>
2019-02-17 16:52:15 +0800

    Update side in commands and remove kernel size for IMG2COL conv

--- FusionAccel/rtl/csb.v
+++ FusionAccel/rtl/csb.v
@@ -16,7 +16,7 @@ module csb(
     input           cmd_fifo_empty,
     input  [6:0]    cmd_size,   //total command size received from okHost after loading memory.
     output          cmd_fifo_rd_en,
-    output          dma_cmd_reads_en,
+    output          dma_p1_reads_en,
 
     output [2:0]    op_type,
     output          padding,
@@ -37,38 +37,21 @@ module csb(
 //TODO: Padding = 1 --> Add 0 in memory
 //TODO: Concatenation Layer
 
-//Compressed Commands from SDRAM
-//|----------CMD TYPE----------|
-//|        op_type:  3Bit      | 1Bit remained space
-//|    padding = 1:  1Bit      | 3Bit remained space
-//|         stride:  4Bit      | 
-//|         op_num: 20Bit      |
-//|  input channel size: 16Bit |
-//| output channel size: 16Bit |
-//|   input kernel size:  8Bit |
-//|  output kernel size:  8Bit |
-//| output surface size: 16Bit |
-//|   weight_start_addr: 32Bit |
-//|     data_start_addr: 32Bit |
-//|  write_back_address: 32Bit |
-//|----------------------------| Totally 192Bit
-
-//|MEM-Block|---------Address---------|---Space--|---Used-Space---|
-//|---------|-------------------------|----------|----------------|
-//|   Cmd   | 0x000_0000 - 0x000_007f |    128   |                |
-//|  Weight | 0x000_1000 - 0x009_D3FF |1280k / 2 |1231552+CONVBIAS|
-//|  Image  | 0x00A_0000 - 0x00B_1F1B | 147k / 2 |                |
-//|  Outbuf | 0x00C_0000 - 0x7ff_ffff | 125M-128 |    3071416     |
-//|---------|-------------------------|----------|----------------|
-
-//|---------------------type------------------------|----op_type----|
-//|IDLE                                             |      000      |
-//|CONV1x1 + ReLU Activation                        |      001      |
-//|CONV3x3 + ReLU Activation                        |      010      |
-//|CONV3x3(with padding) & CONV1x1 + ReLU Activation|      011      |
-//|Max Pooling                                      |      100      |
-//|Average Pooling                                  |      101      |
-//|-------------------------------------------------|---------------| 
+//Compressed Commands from SDRAM    |MEM-Block|---------Address---------|---Space--|---Used-Space---|
+//|----------CMD TYPE----------|    |---------|-------------------------|----------|----------------|
+//|        op_type:  3Bit(1Bit)|    |   Cmd   | 0x000_0000 - 0x000_007f |    128   |                |
+//|    padding = 1:  1Bit(3Bit)|    |  Weight | 0x000_1000 - 0x009_D3FF |1280k / 2 |1231552+CONVBIAS|
+//|         stride:  4Bit      |    |  Image  | 0x00A_0000 - 0x00B_1F1B | 147k / 2 |                |
+//|         op_num: 20Bit      |    |  Outbuf | 0x00C_0000 - 0x7ff_ffff | 125M-128 |    3071416     |
+//|  input channel size: 16Bit |    |---------|-------------------------|----------|----------------|
+//| output channel size: 16Bit |    |---------------------type------------------------|----op_type----|
+//|     input side size:  8Bit |    |IDLE                                             |      000      |
+//|    output side size:  8Bit |    |CONV1x1 + ReLU Activation                        |      001      |
+//| output surface size: 16Bit |    |CONV3x3 + ReLU Activation                        |      010      |
+//|   weight_start_addr: 32Bit |    |CONV3x3(with padding) & CONV1x1 + ReLU Activation|      011      |
+//|     data_start_addr: 32Bit |    |Max Pooling                                      |      100      |
+//|  write_back_address: 32Bit |    |Average Pooling                                  |      101      |
+//|-------Totally 192Bit-------|    |-------------------------------------------------|---------------| 
 
 //Handshake signals to submodules
 reg         conv_ready, maxpool_ready, avepool_ready;
@@ -77,23 +60,24 @@ reg         conv_ready, maxpool_ready, avepool_ready;
 localparam  CMD_BURST_LEN = 3'd6;
 reg         cmd_fifo_rd_en;
 reg [2:0]   cmd_burst_count;
+reg         dma_p1_reads_en;
 
-reg [2:0]   op_type;              //Output
+//Commands
+reg [2:0]   op_type;                    //Output
 reg         padding;
 reg [3:0]   stride;
-reg [19:0]  op_num;              //Output
+reg [19:0]  op_num;                     //Output
 reg [15:0]  i_channel_size, o_channel_size;
-reg [7:0]   i_kernel_size, o_kernel_size;
+reg [7:0]   i_side_size, o_side_size;
 reg [15:0]  o_surf_size;
-reg [31:0]  weight_start_addr;   //Output
-reg [31:0]  data_start_addr;     //Output
-reg [31:0]  result_addr;      //Output
+reg [31:0]  weight_start_addr;          //Output
+reg [31:0]  data_start_addr;            //Output
+reg [31:0]  result_addr;                //Output
 
-reg [15:0]  n_count;             //TODO: n_count from 0 to op_num, step = conv kernel size, // +64 per read = +4 per read per channel
-reg         op_run;                     //Output, indicating p0 transfers command or data
+reg [15:0]  done_surf_count;            // from 0 to o_surf_size
+reg [15:0]  done_channel_count;         // from 0 to o_channel_size, step = PARA = 16
 reg         engine_reset;
 
-reg         dma_cmd_reads_en;
 reg         irq;                        //Output, interrupt signal
 
 //State Machine
@@ -102,7 +86,7 @@ localparam  cmd_collect = 3'b001; //Get command from SDRAM
 localparam  cmd_issue = 3'b010; //Generate DMA access commands
 localparam  wait_op = 3'b011; //Get done signals from submodule macs
 localparam  finish = 3'b100;
-
+// State jump triggers
 reg         cmd_collect_done;
 reg         cmd_issue_done;
 reg         op_done;
@@ -151,11 +135,11 @@ end
 //DMA Accesss commands
 always @ (posedge clk or posedge rst) begin
     if(rst) begin
-        dma_cmd_reads_en <= 0;
+        dma_p1_reads_en <= 0;
     end else begin
-        if(op_en) dma_cmd_reads_en <= 1; //Assert to DMA readout, DMA writing data to FIFO
+        if(op_en) dma_p1_reads_en <= 1; //Assert to DMA readout, DMA writing data to FIFO
         if(cmd_fifo_wr_count == cmd_size * 6) begin
-            dma_cmd_reads_en <= 0;       //Read command
+            dma_p1_reads_en <= 0;       //Read command
         end
     end
 end
@@ -168,25 +152,18 @@ always @ (posedge clk or posedge rst) begin
         //Commands
         op_type <= 3'd0; stride <= 4'h0; padding <= 1'b0; op_num <= 20'h0000;
         i_channel_size <= 16'h0000; o_channel_size <= 16'h0000;
-        i_kernel_size <= 8'h00; o_kernel_size <= 8'h00; o_surf_size <= 16'h0000;
+        i_side_size <= 8'h00; o_side_size <= 8'h00; o_surf_size <= 16'h0000;
         data_start_addr <= 32'h0000_0000;
         weight_start_addr <= 32'h0000_0000;
         result_addr <= 32'h0000_0000;
-        op_run <= 0;
-        n_count <= 16'h0000;
 
-        conv_ready <= 0;
-        maxpool_ready <= 0;
-        avepool_ready <= 0;
+        done_channel_count <= 16'h0000; done_surf_count <= 16'h0000;
+        conv_ready <= 0; maxpool_ready <= 0; avepool_ready <= 0;
+        cmd_collect_done <= 0; cmd_issue_done <= 0; op_done <= 0;
 
-        cmd_collect_done <= 0;
-        cmd_issue_done <= 0;
-        op_done <= 0;
         engine_reset <= 1;
-
         irq <= 0;
-    end
-    else begin
+    end else begin
         case (curr_state)
             idle: begin
                 cmd_burst_count <= CMD_BURST_LEN;
@@ -200,10 +177,10 @@ always @ (posedge clk or posedge rst) begin
                 case (cmd_burst_count)
                     6: begin op_type <= cmd[2:0]; padding <= cmd[4]; stride <= cmd[11:8]; op_num <= cmd[31:12]; end
                     5: begin i_channel_size <= cmd[15:0]; o_channel_size <= cmd[31:16]; end
-                    4: begin i_kernel_size <= cmd[7:0]; o_kernel_size <= cmd[15:8]; o_surf_size <= cmd[31:16]; end
+                    4: begin i_side_size <= cmd[7:0]; o_side_size <= cmd[15:8]; o_surf_size <= cmd[31:16]; end
                     3: begin weight_start_addr <= cmd; end
                     2: begin data_start_addr <= cmd; end
-                    1: begin result_addr <= cmd; cmd_collect_done <= 1; cmd_fifo_rd_en <= 0; op_run <= 1; end
+                    1: begin result_addr <= cmd; cmd_collect_done <= 1; cmd_fifo_rd_en <= 0; end
                     default: ;
                 endcase 
             end
@@ -211,7 +188,7 @@ always @ (posedge clk or posedge rst) begin
                 engine_reset <= 0;
                 cmd_burst_count <= CMD_BURST_LEN;
                 cmd_collect_done <= 0;
-                //Notes: Send out dma access signals (addr) to get data and weight (according to op_type) to submodules (ready signals)
+                //Notes: Send out dma access signals(ready, addr) to submodules according to op_type to get data and weight
                 case (op_type)
                     1,2,3: begin conv_ready <= 1; cmd_issue_done <= 1; end
                     4:begin maxpool_ready <= 1; cmd_issue_done <= 1; end
@@ -220,11 +197,13 @@ always @ (posedge clk or posedge rst) begin
                 endcase
             end
             wait_op: begin
-                //Reset register in cmd_issue
-                cmd_issue_done <= 0;
-                //Wait for submodules to finish --> wait for valid/done signals
-                if(conv_valid | maxpool_valid | avepool_valid) n_count <= n_count + 16; // n parallelism is 16
-                if(n_count + 16 == o_channel_size) begin
+                cmd_issue_done <= 0; //Reset the registers in cmd_issue and wait for submodules to finish
+                if(conv_valid | maxpool_valid | avepool_valid) done_surf_count <= done_surf_count + 1; 
+                if(done_surf_count == o_surf_size) begin
+                    done_channel_count <= done_channel_count + 16; // parallelism is 16
+                    done_surf_count <= 0;
+                end
+                if(done_channel_count + 16 == o_channel_size) begin
                     if(conv_valid) begin conv_ready <= 0; end
                     if(maxpool_valid) begin maxpool_ready <= 0; end
                     if(avepool_valid) begin avepool_ready <= 0;end
@@ -232,7 +211,6 @@ always @ (posedge clk or posedge rst) begin
                 end
             end
             finish: begin
-                op_run <= 0;
                 op_done <= 0;
                 irq <= 1;
             end
