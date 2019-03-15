817a28d892837e7549d5176134e6795fc9d4f478
GeraltShi <paladin.heleth@gmail.com>
2019-03-15 18:43:02 +0800

    add state machine in fsum

--- FusionAccel/rtl/fsum.v
+++ FusionAccel/rtl/fsum.v
@@ -39,40 +39,75 @@ always @(posedge clk) fsum_data_valid <= operation_rfd_fsum;
 always @(posedge clk) fsum_result <= result_fsum;
 always @(posedge clk) fsum_ready <= rdy_fsum;
 
+localparam idle 		= 0;
+localparam busy         = 1;
+
+reg [1:0] curr_state;
+reg [1:0] next_state;
+
+//    Current State, non-blocking
+always @ (posedge clk or posedge rst)    begin
+    if (rst)
+        curr_state    <= idle;
+    else
+        curr_state    <= next_state;
+end
+
+//    Status Jump, blocking
+always @ (*) begin
+    next_state = idle;    //    Initialize
+    case (curr_state)
+        idle: begin
+            if(!fifo_empty) next_state = busy;
+            else next_state = idle;
+        end
+		busy: begin
+            if(ready) next_state = idle;
+            else next_state = busy;
+        end
+        default: next_state = idle;
+    endcase
+end
+
 always @ (posedge clk or posedge rst) begin
 	if(rst) begin
         fsum_data_ready <= 0; fsum_a <= 16'h0000; fsum_b <= 16'h0000; fsum_count <= 8'h00; ready <= 0; reads_en <= 0;
     end else begin
-        if(!fifo_empty) begin
-            reads_en <= 1;
-            fsum_count <= 0;
-        end
-        if(valid) begin 
-            fsum_data_ready <= 1; 
-            reads_en <= 0; 
-            if(i_channel_count == 0) fsum_a <= bias;
-            else fsum_a <= fsum[fsum_index];
-            fsum_b <= data[16*fsum_count +: 16];
-        end
-        if(fsum_data_ready) begin
-            fsum_data_ready <= 0;
-            if(fsum_count < `BURST_LEN) begin
-                fsum_count <= fsum_count + 1;
-            end else begin
+        case(curr_state)
+            idle: begin
+                if(!fifo_empty) reads_en <= 1;
                 fsum_count <= 0;
             end
-        end
-        if(fsum_ready) begin
-            fsum_a <= result_fsum;
-            if(fsum_count < `BURST_LEN) begin
+            busy: begin
+            if(valid) begin 
+                fsum_data_ready <= 1; 
+                reads_en <= 0; 
+                if(i_channel_count == 0) fsum_a <= bias;
+                else fsum_a <= fsum[fsum_index];
                 fsum_b <= data[16*fsum_count +: 16];
-                fsum_data_ready <= 1;
-            end else begin
-                fsum_b <= 0;
-                ready <= 1;
             end
-        end
-        if(ready) ready <= 0;
+            if(fsum_data_ready) begin
+                fsum_data_ready <= 0;
+                if(fsum_count < `BURST_LEN) begin
+                    fsum_count <= fsum_count + 1;
+                end else begin
+                    fsum_count <= 0;
+                end
+            end
+            if(fsum_ready) begin
+                fsum_a <= result_fsum;
+                if(fsum_count < `BURST_LEN) begin
+                    fsum_b <= data[16*fsum_count +: 16];
+                    fsum_data_ready <= 1;
+                end else begin
+                    fsum_b <= 0;
+                    ready <= 1;
+                end
+            end
+            if(ready) ready <= 0;
+            end
+        
+        endcase
     end
 end
 endmodule
